ea2f2e2528b7ae18743fe91cd4160542f69fbcd8
esramm <esra_cetinkaya33@hotmail.com>
2017-04-10 12:48:13 +1000

    final

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ $( document ).ready(function() {
      $(".burger-menu").slideToggle();
    });
 });
+
+
+
+// SMOOTH SCROLL JS
+
+// // Bind this action to all "A" links
+// $(document).on('click', 'a', function(event){
+//     // Stop the browser from automatically jumping to the target
+//     event.preventDefault();
+//
+//     // Get the offest location of the target element
+//     $('html, body').animate({
+//         // Smoothly Move To That Offset
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 1500);
+// });
