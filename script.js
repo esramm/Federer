2f2197cd6b640dd200af0ec2a457edbc15f9f5ce
esramm <esra_cetinkaya33@hotmail.com>
2017-04-10 11:46:24 +1000

    testing for snipcart

---
+++ script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+   $(".burger-button").click(function(){
+     $(".burger-button").toggleClass("active");
+     $(".burger-menu").slideToggle();
+   });
+   $(".burger-menu").click(function(){
+     $(".burger-button").toggleClass("active");
+     $(".burger-menu").slideToggle();
+   });
+});
